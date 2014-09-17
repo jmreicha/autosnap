544537d9a21c6bdef2d2cefbf2c762c3a0fe8da2
Josh Reichardt <josh.reichardt@gmail.com>
2014-09-16 22:49:58 -0500

    Add stub options for cmd

--- autosnap/__init__.py
+++ autosnap/__init__.py
@@ -1,2 +1,3 @@
+import logging
 
 __version__ = '0.0.1'
--- autosnap/cmd.py
+++ autosnap/cmd.py
@@ -6,7 +6,16 @@ class Main(Command):
 
     def set_opts(self):
         """Command line options"""
-        self.opt('--version', action='version', version=autosnap.__version__)
+        self.opt('--volume', '-v', help='list managed volumes')
+        self.opt('--snapshot', '-s', help='list snapshots')
+        self.opt('--log-level', '-l', help='set logging level')
+        self.opt('--config', '-c',
+                help='override config file (default is ../.config)')
+        self.opt('--region', '-r',
+                help='override region (defaults to config file region)')
+        self.opt('--dry-run', '-d',
+                help='list snapshots that will be taken, does not create them')
+        self.opt('--version' action='version', version=autosnap.__version__)
 
     def run(self):
-        server.run_server(port=self.args.port, host=self.args.host)
+        #server.run_server(port=self.args.port, host=self.args.host)
